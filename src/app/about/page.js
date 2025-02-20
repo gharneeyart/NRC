7b5bdeee0bc244934e5d39b3b11fcbbd1acb8c91
APPLE <apple@APPLEs-MacBook-Pro-2.local>
2025-02-20 16:08:46 +0100

    update

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,13 +1,17 @@
+import Count from "@/components/about/count";
 import ResuableHero from "@/components/reusables/hero";
 import Image from "next/image"
 
 
 export default function About() {
     return(
-        <div className="about-hero ">
-            <div className="container mx-auto ">
+        <div className=" ">
+          <div className="about-hero">
+          <div className=" container mx-auto ">
             <ResuableHero title='About NRC' desc='Safe, comfortable, and reliable train services across the nation. Experience the future of Nigerian transportation, today.'/>
             </div> 
+          </div>
+            <Count/>
         </div>
         
     )
--- src/components/about/count.js
+++ src/components/about/count.js
@@ -1,3 +1,20 @@
 export default function Count() {
-    
-}
+    return(
+        <div className="bg-[#006B14] text-white flex flex-col lg:flex-row justify-between px-[167px]">
+            <div className="flex lg:flex-row flex-col items-center ">
+                <h1 className="font-[700] text-[54px]">100k+</h1>
+               <p className="flex flex-row lg:flex-col font-[20px] text-left w-28">Happy Customers</p>
+            </div>
+            <div className="flex lg:flex-row flex-col items-center ">
+            <h1 className="font-[700] text-[54px]">140+</h1>
+            <p className="flex flex-row lg:flex-col font-[20px] text-left w-28">Routes Covered</p>
+            </div>
+            <div>
+                <span><h1>140+</h1>
+                <p>Routes Covered</p>
+                </span>
+            </div>
+        </div>
+        
+    )
+} 

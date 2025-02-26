1d5d91b7099304371069d206963bdfca9ec59c38
Chibuike Nnorom <nnoromchibuikepromise@gmail.com>
2025-02-26 14:39:57 +0100

    Edited search & Hero

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,21 @@
-<<<<<<< HEAD
 import Hero from "@/components/home/hero";
 import Search from "@/components/home/search";
-
-
-export default function Home() {
-  return (
-    <div className="relative">
-     <div>
-       <Hero />
-     </div>
-      <div className="w-full absolute -bottom-[22.5rem] md:-bottom-[22rem] lg:-bottom-20" >
-        <Search/>
-      </div>
-=======
 import Explore from "@/components/about/explore";
 
 export default function Home() {
   return (
     <div>
+     <div className="relative">
+      <div>
+        <Hero />
+      </div>
+        <div className="w-full absolute -bottom-[22.5rem] md:-bottom-[22rem] lg:-bottom-20" >
+          <Search/>
+        </div>
+     </div>
+
       <Explore/>
     
->>>>>>> 5bfcfee43da9c1076bf4c15fc22b27ddc251c112
     </div>
   );
 }

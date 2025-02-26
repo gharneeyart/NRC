32da02b9209d5d9ed4606053eb3209c758284aee
Chibuike Nnorom <nnoromchibuikepromise@gmail.com>
2025-02-26 11:49:56 +0100

    Hero section & Search section

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,16 @@
+import Hero from "@/components/home/hero";
+import Search from "@/components/home/search";
 
 
 export default function Home() {
   return (
-    <div>
-      <h1>Home Page</h1>
-    
+    <div className="relative">
+     <div>
+       <Hero />
+     </div>
+      <div className="w-full absolute -bottom-[22.5rem] md:-bottom-[22rem] lg:-bottom-20" >
+        <Search/>
+      </div>
     </div>
   );
 }

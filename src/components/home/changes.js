309fed9a57e5b00c424699dc384120d6608675ff
Akinsola Olajide <akinsolaolajide150@gmail.com>
2025-03-01 16:25:40 +0000

    updates, added two files too

--- src/components/home/changes.js
+++ src/components/home/changes.js
@@ -1,35 +1,10 @@
-import Image from "next/image"
+import Card from "../reusables/cards/changesCard"
+import Ticket from "../reusables/cards/e-tickettingCard"
 export default function Changes() {
     return(
-        <div className="w-11/12 container   flex flex-col lg:flex-row justify-center xl:gap-[5rem] mx-auto gap-[1.691rem]  mt-[14rem] lg:mt-[4.506rem]  ">
-          <section className="relative">
-            <Image className="w-full h-full rounded-[0.432rem] lg:rounded-[0.77rem]" src="/images/Frame 40989 (1).svg" width={100} height={100} alt="train-times"/>
-            <div className="xl:w-[27.605rem] flex flex-col gap-[1.547rem] lg:gap-[2.759rem] text-[#263238] absolute top-0 px-[1.043rem] lg:px-[1.861rem] pt-[2.087rem] lg:pt-[3.722rem]">
-              <div className="space-y-[0.36rem] lg:space-y-[0.642rem]">
-                <h1 className="text-[1.125rem] lg:text-[2.053rem] font-bold">Changes to Train Times</h1>
-                <p className="text-[0.75rem] lg:text-[1.283rem] font-semibold">Check your routes before traveling</p>
-              </div>
-                <div className="text-[0.75rem] lg:text-[0.955rem] font-bold">
-                  <button className="bg-white px-[1.188rem] lg:px-[1.524rem] py-[0.636rem] lg:py-[1.134rem] rounded-[0.252rem] lg:rounded-[0.449rem]">Know More</button>
-                </div>
-            </div>
-            </section>
-
-            <section className="relative">
-                <Image className="w-full h-full rounded-[0.432rem] lg:rounded-[0.77rem]" src="/images/Frame 40990 (1).svg" width={100} height={100} alt="E-ticketing"/>
-
-                <div className="w-[15.476rem] lg:w-[27.605rem]  flex flex-col gap-[0.245rem] lg:gap-[1.7rem] text-white absolute top-0 px-[1.043rem] lg:px-[1.861rem] pt-[2.087rem] lg:pt-[3.722rem]">
-                    <div className="space-y-[0.36rem] lg:space-y-[0.642rem]">
-                        <h1 className="text-[1.125rem] lg:text-[2.053rem] font-bold">Try E-Ticketing</h1>
-                        <p className="text-[0.75rem] lg:text-[1.283rem] font-semibold">A more convenient way to get your ticket
-                        </p>
-                    </div>
-
-                    <div className="text-[0.75rem] lg:text-[0.955rem] text-[#263238] font-bold">
-                        <button className="bg-white px-[1.188rem] lg:px-[1.524rem] py-[0.636rem] lg:py-[1.134rem] rounded-[0.252rem] lg:rounded-[0.449rem]">Know More</button>
-                    </div>
-                </div>
-            </section>
-        </div>
+       <div className="grid grid-cols-1 mt-[3.25rem] mb-[4.55rem] lg:mt-[4.506rem] lg:mb-[9.434rem] gap-[1.691rem] lg:gap-[3.016rem] lg:grid-cols-2 container w-11/12 mx-auto">
+        <Card/>
+        <Ticket/>
+       </div> 
     )
 }
--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -15,7 +15,7 @@ export default function Nav() {
     }
 
     return (
-        <div className="w-full   bg-white py-[2.49rem]">
+        <div className="w-full bg-white py-[2.49rem]">
             <div className="w-11/12 container mx-auto flex items-center justify-between">
             <Link href="/">
                 <Image className="w-[8.893rem] h-[3.344rem]" src="/icons/image 3.svg" alt="NRC-logo" width={100} height={100} priority={true} />
@@ -25,9 +25,9 @@ export default function Nav() {
                 {menuOpen ? <IoCloseOutline /> : <RxHamburgerMenu className="text-[#0C4C51]"/>}
             </div>
 
-            <div className={`lg:static  text-white text-[18.6px] flex flex-col   gap-[3.375rem] top-24 rounded-sm lg:flex  ${
+            <div className={`lg:static container text-white text-[18.6px] flex flex-col   gap-[3.375rem] top-24 rounded-sm lg:flex  ${
   menuOpen ? 'w-full block opacity-100 bg-white right-0 p-8 ' : 'hidden opacity-0 top-[100%] py-1'
-}  lg:block absolute lg:opacity-100 lg:flex-row lg:w-auto justify-between  font-semibold  z-50   xl:gap-[10rem] `}>
+}  lg:block absolute lg:opacity-100 lg:flex-row lg:w-auto justify-between  font-semibold  z-50 lg:gap-[3rem]  xl:gap-[12rem] 2xl:gap-[20rem]`}>
                 <div className="flex flex-col lg:flex-row items-center gap-[1.625rem] xl:gap-[2.2rem] text-[18.6px] text-[#263238] lg:text-[#01320A] font-sans">
                     <Link href="/"><h1>Home</h1></Link>
                     <Link href="/about">About</Link>
@@ -36,9 +36,9 @@ export default function Nav() {
                     <Link href="/contact"><h1>Contact Us</h1></Link>
                 </div>
 
-                <div className="flex flex-col  gap-[1.75rem] lg:flex-row lg:gap-[2.4rem]">
+                <div className="flex flex-col text-center  gap-[1.75rem] lg:flex-row lg:gap-[2.4rem]">
                     <Link href="/signup" className="border border-[#18A532] text-[#18A532] py-2 px-6 rounded-md">Register</Link>
-                    <Link href="/login"><button className="text-white bg-[#18A532] py-2 px-6 rounded-md">Sign in</button></Link>
+                    <Link href="/login"><button className="w-full text-white bg-[#18A532] py-2 px-6 rounded-md">Sign in</button></Link>
                 </div>
             </div>
             </div>

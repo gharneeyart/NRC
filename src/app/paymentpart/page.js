889a7d21d7113f4631618ba3091523544ecb462e
Akinsola Olajide <akinsolaolajide150@gmail.com>
2025-03-10 12:00:04 +0000

    updates

--- src/app/paymentpart/page.js
+++ src/app/paymentpart/page.js
@@ -1,6 +1,7 @@
 import ApplyVoucher from "@/components/details/applyVoucher"
 import ContactDetail from "@/components/details/contactDetails"
 import PassengerDetails from "@/components/details/passengerDetails"
+import UserAgreement from "@/components/details/userAgreement"
 import MorningTrain from "@/components/reusables/morningTrain"
 export default function Payment() {
   return (
@@ -16,22 +17,7 @@ export default function Payment() {
         </div>
         <ApplyVoucher/>
       </div>
-
-      <div className="container w-full flex flex-col gap-[45.43px] lg:gap-[52px] mt-[36.57px] lg:mt-[31px] ">
-        <div className="flex gap-[9px] text-[14px] lg:text-lg px-[0.703rem] lg:px-[1.878rem]">
-          <div>
-            <input className="accent-[#18A532]" type="checkbox"></input>
-          </div>
-          <div className="w-full lg:w-[500.47px] xl:w-[560.57px]">
-            <h1>By proceeding with the booking, I confirm that I have read and accepted the Privaccy Policy, User Agreement and Terms of Service</h1>
-          </div>
-        </div>
-
-        <div className="flex flex-col lg:flex-row gap-[23px] lg:gap-[49px] px-[0.703rem] lg:px-[1.878rem]">
-          <button className="px-[94px] py-[22px] text-center rounded-[4.65px] bg-[#18A532] text-white lg:text-lg">Make Payment</button>
-          <button className="px-[127px] py-[22px] text-center rounded-[4.65px] lg:text-lg text-[#18A532] border border-[#18A532]">Cancel</button>
-        </div>
-      </div>
+      <UserAgreement/>
     </div>
   )
 }

888d0b9237b755cc27a9d3aaf4b940e167afdd26
Akinsola Olajide <akinsolaolajide150@gmail.com>
2025-03-24 14:23:11 +0000

    update

--- src/layouts/Nav.js
+++ src/layouts/Nav.js
@@ -1,119 +1,77 @@
-'use client';
-import Image from 'next/image';
-import { RxHamburgerMenu } from 'react-icons/rx';
-import { IoCloseOutline } from 'react-icons/io5';
-import { useState, useEffect, useRef } from 'react';
-import Link from 'next/link';
-import { RiArrowDropDownLine } from 'react-icons/ri';
-import Logo from '/public/icons/image 3.svg';
+'use client'
+import Image from "next/image"
+import { RxHamburgerMenu } from "react-icons/rx";
+import { IoCloseOutline } from "react-icons/io5";
+import { useState, useEffect, useRef } from "react";
+import Link from "next/link";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import Logo from '/public/icons/image 3.svg'
 
 export default function Nav() {
-  const [menuOpen, setMenuOpen] = useState(false);
-  const navRef = useRef(null);
-
-  const closeMenubar = () => {
-    setMenuOpen(false);
-  };
-
-  const openMenubar = () => {
-    if (window.innerWidth <= 1024) {
-      // Check if the screen width is less than 1024px (mobile devices)
-      setMenuOpen(!menuOpen);
+    const [menuOpen, setMenuOpen] = useState(false);
+    const navRef = useRef(null)
+    const closeMenubar = () => {
+        setMenuOpen(false)
     }
-  };
-
-  useEffect(() => {
-    const handleClickOutside = (event) => {
-      if (navRef.current && !navRef.current.contains(event.target)) {
-        closeMenubar();
-      }
-    };
-
-    if (menuOpen) {
-      document.addEventListener('mousedown', handleClickOutside);
-    } else {
-      document.removeEventListener('mousedown', handleClickOutside);
+    
+    const openMenubar = () =>{
+    if (window.innerWidth <= 1024) { // Check if the screen width is less than 1024px (mobile devices)
+        setMenuOpen(!menuOpen);
     }
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside);
-    };
-  }, [menuOpen]);
+}
+// useEffect(() => {
+//     const handleClickOutside = (event) => {
+//         if (navRef.current && !navRef.current.contains(event.target)) {
+//             closeMenubar();
+//         }
+//     };
 
-  const [dropOpen, setDropOpenMenu] = useState(false);
-  const openMenu = () => {
+//     if (menuOpen) {
+//         document.addEventListener("mousedown", handleClickOutside);
+//     } else {
+//         document.removeEventListener("mousedown", handleClickOutside);
+//     }
+//     return () => {
+//         document.removeEventListener("mousedown", handleClickOutside);
+//     };
+// }, [menuOpen]);
+
+
+const [dropOpen, setDropOpenMenu] = useState(false);
+const openMenu = () => {
     setDropOpenMenu(!dropOpen);
-  };
-  const closeMenu = () => {
+}
+const closeMenu = () => {
     setDropOpenMenu(false);
-  };
+}
 
-  useEffect(() => {
-    const clickOutside = (event) => {
-      if (navRef.current && !navRef.current.contains(event.target)) {
-        closeMenu();
-      }
-    };
+// const clickOutside = (event) => {
+//     if (navRef.current && !navRef.current.contains(event.target)) {
+//         closeMenu();
+//     }
+// }
 
-    if (dropOpen) {
-      document.addEventListener('mousedown', clickOutside);
-    } else {
-      document.removeEventListener('mousedown', clickOutside);
-    }
-    return () => {
-      document.removeEventListener('mousedown', clickOutside);
-    };
-  }, [dropOpen]);
+// useEffect(() => {
+//     if (dropOpen) {
+//         document.addEventListener("mousedown", clickOutside);
+//     } else {
+//         document.removeEventListener("mousedown", clickOutside);
+//     }
+//     return () => {
+//         document.removeEventListener("mousedown", clickOutside);
+//     };
+// }, [dropOpen]);
+    
 
-  return (
-    <div className="w-full bg-white py-[2.49rem] fixed top-0 left-0 right-0 z-50 border border-[#D8D8D8]">
-      <div className="w-11/12 container mx-auto flex items-center justify-between">
-        <Link href="/">
-          <Image
-            className="w-[8.893rem] h-[3.344rem]"
-            src={Logo}
-            alt="NRC-logo"
-            width={100}
-            height={100}
-            priority={true}
-          />
-        </Link>
 
-        <div
-          className="lg:hidden text-2xl  px-4 cursor-pointer"
-          onClick={openMenubar}
-        >
-          {menuOpen ? (
-            <IoCloseOutline />
-          ) : (
-            <RxHamburgerMenu className="text-[#0C4C51]" />
-          )}
-        </div>
+//The mousedown event is used to detect when a mouse button is pressed down. 
+// The return statement provides a cleanup function that removes the handleClickOutside event listener when the component unmounts or before the effect re-runs.
 
-        <div
-          className={`lg:static container text-white text-[18.6px] flex flex-col gap-[3.375rem] top-24 rounded-sm lg:flex ${
-            menuOpen
-              ? 'w-full block opacity-100 bg-white right-0 p-8 '
-              : 'hidden opacity-0 top-[100%] py-1'
-          }  lg:block absolute lg:opacity-100 lg:flex-row lg:w-auto justify-between  font-semibold h-screen lg:h-full  z-50 lg:gap-[3rem]  xl:gap-[12rem] 2xl:gap-[20rem]`}
-        >
-          <div
-            className="flex flex-col lg:flex-row items-center gap-[1.625rem] xl:gap-[2.2rem] text-[18.6px] text-[#263238] lg:text-[#01320A] font-sans "
-            ref={navRef}
-          >
+    return (
+        <div className="w-full bg-white py-[2.49rem] fixed top-0 left-0 right-0 z-50 border border-[#D8D8D8] active:bg-gray-200 ">
+            <div className="w-11/12 container mx-auto flex items-center justify-between">
             <Link href="/">
-              <h1 className="hover:text-[#006B14]">Home</h1>
-            </Link>
-            <Link href="/about">
-              <h1 className="hover:text-[#006B14]">About</h1>
-            </Link>
-            <Link href="/timetables">
-              <h1 className="hover:text-[#006B14]">Time Tables</h1>
-            </Link>
-            <Link href="/faq">
-              <h1 className="hover:text-[#006B14]">FAQs</h1>
-            </Link>
-            <Link href="/contact">
-              <h1 className="hover:text-[#006B14]">Contact Us</h1>
+                <Image className="w-[8.893rem] h-[3.344rem]" src={Logo} alt="NRC-logo" width={100} height={100} priority={true} />
             </Link>
 
             <div className="lg:hidden text-2xl  px-4 cursor-pointer" onClick={openMenubar}>
@@ -130,7 +88,7 @@ export default function Nav() {
                     <Link href="/faq"><h1 className="hover:text-[#006B14]">FAQs</h1></Link>
                     <Link href="/contact"><h1 className="hover:text-[#006B14]">Contact Us</h1></Link>
 
-                    <div className="w-[198px] bg-whitej mx-auto items-center justify-center relative block lg:hidden active:bg-[#f0f4f1] rounded-full  cursor-pointer"onClick={openMenu}>
+                    <div className="w-[198px] bg-whitej mx-auto items-center justify-center relative block lg:hidden hover:bg-[#f0f4f1] hover:shadow-lg rounded-full  cursor-pointer active:bg-gray-200"onClick={openMenu}>
                     <div className="flex gap-[8px] items-center">
                         <div className="h-[51px] w-[51px] flex items-center justify-center text-lg text-[#006B14] text-center rounded-full bg-[#F0F7F6]"><h1>M.K</h1></div>
                         <div className="text-base text-[#3C3C3C]"><h3>Michael Kay</h3></div>
@@ -158,7 +116,7 @@ export default function Nav() {
                     <Link href="/auth/login"><button className="w-full text-white bg-[#18A532] py-2 px-6 rounded-md">Sign in</button></Link>
                 </div> */}
                 
-                <div className="w-[198px] bg-white  gap-[23px] mx-auto items-center justify-center relative  hidden lg:block active:bg-[#f0f4f1] rounded-full  cursor-pointer" onClick={openMenu}>
+                <div className="w-[198px] bg-white  gap-[23px] mx-auto items-center justify-center relative  hidden lg:block hover:bg-[#f0f4f1] hover:shadow-lg rounded-full  cursor-pointer active:bg-gray-200" onClick={openMenu}>
                     <div className="flex gap-[8px] items-center">
                         <div className="h-[51px] w-[51px] flex items-center justify-center text-lg text-[#006B14] text-center rounded-full bg-[#F0F7F6]"><h1>M.K</h1></div>
                         <div className="text-base text-[#3C3C3C]"><h3>Michael Kay</h3></div>
@@ -184,7 +142,5 @@ export default function Nav() {
             
             </div>
         </div>
-      </div>
-    </div>
-  );
+    )
 }
